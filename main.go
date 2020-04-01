8dca8bb2ca4fec512c9172ed532bb2923fc817fc
cixo <find1set@gmail.com>
2020-04-01 13:52:48 +0800

    Load arguments

--- main.go
+++ main.go
@@ -7,7 +7,13 @@ import (
 )
 
 func main() {
+	args := os.Args
 	pwd, _ := os.Getwd()
 	host, _ := os.Hostname()
 	fmt.Printf("🌋: Hello World!\n💻: %s\n📂: %s\n⏰: %s\n", host, pwd, time.Now().Format("2006-01-02T15:04:05-0700"))
+
+	fmt.Printf("\ngot arguments: %d\n", len(args))
+	for i, a := range args {
+		fmt.Printf("#%d arg: [%v]\n", i, a)
+	}
 }
